02f1d18d7843b314f04e10c02964970a65ddf6b1
DataHamster <robin.mithoff@gmail.com>
2020-01-24 17:22:15 +0100

    first try

--- bitClock.py
+++ bitClock.py
@@ -1,19 +1,33 @@
 import time
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BMC)
+
+def decimalToBinary(n):
+    return bin(n).replace("0b", "")
+
+
+def bitSignal(dictionary, timeUnit, startIndex):
+
+    i = startIndex
+
+    for item in (decimalToBinary(timeUnit)[::-1]):
+        if item == 1:
+            GPIO.output(dictionary["l"+str(startIndex)], 1)
+            i += 1
+        elif item == 0:
+            GPIO.output(dictionary["l"+str(startIndex)], 0)
+            i += 1
+
+
+GPIO.setmode(GPIO.BCM)
+
 
 gpio_dict = {"l0": 4, "l1": 17, "l2": 27, "l3": 22, "l4": 5, "l5": 6, "l6": 13, "l7": 19,
              "l8": 26, "l9": 21, "l10": 20, "l11": 16, "l12": 12, "l13": 25, "l14": 24, "l15": 23, "l16": 18}
 
-H = time.strftime("%H %M %S").split(sep=" ")
 
-
-for element in gpio_dict.values():
-    GPIO.setup(element, GPIO.OUT)
-
-for i in range(20):
-    for element in gpio_dict.values():
-        GPIO.output(element, 1)
-        time.sleep(0.1)
-        GPIO.output(element, 0)
+while True:
+    Hr, Min, Sec = time.strftime("%H %M %S").split(sep=" ")
+    bitSignal(gpio_dict, Hr, 0)
+    bitSignal(gpio_dict, Min, 5)
+    bitSignal(gpio_dict, Sec, 11)
