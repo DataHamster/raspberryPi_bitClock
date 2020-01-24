589348125978c35736ab02d8a579dbf61ef39c76
DataHamster <robin.mithoff@gmail.com>
2020-01-24 17:46:37 +0100

    second try

--- bitClock.py
+++ bitClock.py
@@ -11,12 +11,10 @@ def bitSignal(dictionary, timeUnit, startIndex):
     i = startIndex
 
     for item in (decimalToBinary(int(timeUnit))[::-1]):
-        print("Startitem: " + str(i))
-        print(item)
-        if item == 1:
+        if item == "1":
             GPIO.output(dictionary["l"+str(startIndex)], 1)
             i += 1
-        elif item == 0:
+        elif item == "0":
             GPIO.output(dictionary["l"+str(startIndex)], 0)
             i += 1
 
@@ -30,8 +28,6 @@ gpio_dict = {"l0": 4, "l1": 17, "l2": 27, "l3": 22, "l4": 5, "l5": 6, "l6": 13, 
 
 while True:
     Hr, Min, Sec = time.strftime("%H %M %S").split(sep=" ")
-    print(Hr, Min, Sec)
     bitSignal(gpio_dict, Hr, 0)
     bitSignal(gpio_dict, Min, 5)
     bitSignal(gpio_dict, Sec, 11)
-    time.sleep(1.0)
